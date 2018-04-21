c74d44feb0f5c3c0ddac4a4b2f6f8965264a40fd
rwq <rlwijma@gmail.com>
2018-04-21 16:21:32 +0200

    Genetic Algo for feature selection

--- assignment1_ruth/ea.py
+++ assignment1_ruth/ea.py
@@ -1,12 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-class individual:
+class Individual:
 
 	def mutate(self):
 		m = np.random.uniform(size=self.n)
 		m = m < self.prob_mutate
 		
-		for i in len(child):
+		for i in range(len(self.genotype)):
 			if m[i]:
 				if self.genotype[i] == 0:
 					self.genotype[i] = 1
@@ -19,22 +20,25 @@ class individual:
 		self.fitness = np.sum(self.genotype)
 
 
+	def set_genotype(self, genotype):
+		self.genotype = genotype
+
+
 	def __init__(self, size):
 		self.n = size
 		self.genotype = np.random.randint(2, size=self.n)
 		self.fitness = None
+		self.prob_mutate = 0.01
 
 
 
 
 class genetic_algorithm:
 
-
-
-
 	def mutation(self, children):
-		for i in len(children):
+		for i in range(len(children)):
 			children[i].mutate()
+			children[i].evaluate()
 
 		return children
 
@@ -43,16 +47,16 @@ class genetic_algorithm:
 		# determine crossover point
 		choice = np.random.uniform(size=self.n) < .5
 
-		child1 = np.zeros(self.n)
-		child2 = np.zeros(self.n)
+		child1 = Individual(self.n)
+		child2 = Individual(self.n)
 
 		for i in range(self.n):
 			if choice[i]:
-				child1[i] = p1[i]
-				child2[i] = p2[i]
+				child1.genotype[i] = p1.genotype[i]
+				child2.genotype[i] = p2.genotype[i]
 			else:
-				child1[i] = p2[i]
-				child2[i] = p1[i]
+				child1.genotype[i] = p2.genotype[i]
+				child2.genotype[i] = p1.genotype[i]
 
 		return child1, child2
 
@@ -62,51 +66,77 @@ class genetic_algorithm:
 		if len(parents) % 2 != 0:
 			raise Exception('Nr of parents should be even')
 
-		for i in range(len(parents)/2):
+		for i in range(int(len(parents)/2)):
 			p1 = parents[i*2]
 			p2 = parents[i*2+1]
 
-			c1, c2 = uniform_crossover(p1, p2) 
+			c1, c2 = self.uniform_crossover(p1, p2) 
 
 			children.append(c1)
 			children.append(c2)
-
-
 
 		return children
 
 
 	def parent_selection(self):
 		# fitness proportate selection using Stochastic Universal Sampling (SUS)
+		fitnesses = [g.fitness for g in self.population]
+		i = 0
+		current = 0
+		r = np.random.uniform(0, 1/self.lmbda)
+
+		parents = []
+
+		while current < self.lmbda:
+			while r <= fitnesses[i]:
+				parents.append(self.population[i])
+				r = r + 1/self.lmbda
+				current += 1
+
+			i += 1
+
+		return parents
+
+
+		
 
 
 
 	def survivor_selection(self, children):
 		# use ranking based selection
+
+		self.population.extend(children)
+
 		self.population.sort(key=lambda x: x.fitness, reverse=True)
 
 		# update fitness
 		for individual in self.population:
 			individual.evaluate()
 
-		# return best 50 from population
-		parents = self.population[:50]
 
-		return parents
+		# return best 50 from population
+		self.population = self.population[:self.mu]
 		
 
 	def evolutionary_cycle(self):
 		# parent selection
+		for individual in self.population:
+			individual.evaluate()
+
 		parents = self.parent_selection()
 
 		# recombination
 		children = self.recombination(parents)
 
+
+
 		# mutation
 		children = self.mutation(children)
 
 		# survivor selection
-		new_gen = self.survivor_selection(children)
+		self.survivor_selection(children)
+
+		self.update_best_individual()
 
 
 
@@ -117,20 +147,40 @@ class genetic_algorithm:
 		population = []
 
 		for i in range(self.mu):
-			genotype = individual(self.n)
+			genotype = Individual(self.n)
 			population.append(genotype)
 
 		return population
 
 
+	def update_best_individual(self):
+		if len(self.fitnesses) == 0:
+			self.best_genotype = self.population[0].genotype
+
+		elif self.population[0].fitness > self.fitnesses[-1]:
+			self.best_genotype = self.population[0].genotype
+
 
 
 	def __init__(self):
 		self.mu = 100
-		self.num_gen = 10
+		self.lmbda = 20
+		self.num_gen = 20
 		self.n = 18
-		self.prob_mutate = 0.01
 		self.population = self.init_population()
+		self.fitnesses = []
+		self.best_genotype = None
 
 		for i in range(self.num_gen):
+			print('generation {}'.format(i))
 			self.evolutionary_cycle()
+
+			self.fitnesses.append(self.population[0].fitness)
+
+		print(self.best_genotype)
+		plt.plot(self.fitnesses)
+		plt.show()
+
+
+
+genetic_algorithm()
